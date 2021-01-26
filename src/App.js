dd6e32fa46582980055fdf88bbfd6d146a9d4a51
Christian-Dungca <cdungca14@gmail.com>
2021-01-26 10:10:12 +0000

    Created a password validation micro interaction
    The micro interaction shakes a form input and changes the color of it when the input is invalid. No real validation, just styling and animations.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [hasError, setError] = useState(false);
+
+  const stylesWrapper = ["wrapper"];
+  const stylesLabel = [];
+
+  const formHandler = (event) => {
+    event.preventDefault();
+    setError(!hasError);
+  };
+
+  if (hasError) {
+    stylesWrapper.push("invalid");
+    stylesLabel.push("invalid");
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="title"> Form Validator ~ Shake Effect</h1>
+      <form onSubmit={formHandler}>
+        <label for="password">
+          {!hasError ? (
+            <h2 className={stylesLabel.join(" ")}> Password </h2>
+          ) : (
+            <h2 className={stylesLabel.join(" ")}> Password is Invalid </h2>
+          )}{" "}
+        </label>
+        <div className={stylesWrapper.join(" ")}>
+          <input type="password" id="password" />
+          <button className="button" type="submit">
+            {!hasError ? <i> &rarr; </i> : <h3> X </h3>}
+          </button>
+        </div>
+      </form>
     </div>
   );
 }
